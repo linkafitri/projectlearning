9778a97f816b7cfc02c51399a632f5b12523c775
putrinursakinah <putricantip3013@gmail.com>
2022-12-06 06:16:51 +0700

    crud paket

--- resources/views/backend/paket/add_paket.blade.php
+++ resources/views/backend/paket/add_paket.blade.php
@@ -13,60 +13,53 @@
 		 <!-- Basic Forms -->
 		  <div class="box">
 			<div class="box-header with-border">
-			  <h4 class="box-title">Tambah User</h4>
+			  <h4 class="box-title">Tambah Paket</h4>
 			  
 			</div>
 			<!-- /.box-header -->
 			<div class="box-body">
 			  <div class="row">
 				<div class="col">
-					<form method="post" action="{{route('users.store')}}">
+					<form method="post" action="{{route('paket.store')}}">
                     @csrf
+					<div class="row">
+                            <div class="col-md-6">
+                            <div class="form-group">
+								<h5>Nama Paket <span class="text-danger">*</span></h5>
+								<div class="controls">
+									<input type="text" name="namapaket" class="form-control" required data-validation-required-message="This field is required"> </div>
+							</div>
+                            </div>
+                            
+                            <div class="col-md-6">
+                            <div class="form-group">
+								<h5>Deskripsi <span class="text-danger">*</span></h5>
+								<div class="controls">
+									<input type="text" name="deskripsi" class="form-control" required data-validation-required-message="This field is required"> </div>
+							</div>
+                            </div>
+
+						<div class="col-md-6">
+                            <div class="form-group">
+								<h5>Harga Paket <span class="text-danger">*</span></h5>
+								<div class="controls">
+									<input type="text" name="hargapaket" class="form-control" required data-validation-required-message="This field is required"> </div>
+							</div>
+                            </div>
+
 						<div class="row">
-							<div class="col-md-6">
+                            <div class="col-md-6">
                             <div class="form-group">
-								<h5>Grup User <span class="text-danger">*</span></h5>
+								<h5>Foto <span class="text-danger">*</span></h5>
 								<div class="controls">
-									<select name="selectUser" id="selectUser" required class="form-control">
-										<option value="">Pilih Grup User</option>
-										<option value="admin">admin</option>
-										<option value="user">user</option>
-
-									</select>
-								</div>
+									<input type="file" name="foto" class="form-control" required data-validation-required-message="This field is required"> </div>
 							</div>
-                        <!-- end basic select -->
-							</div>
-                            <!-- end colmd6 -->
-							<div class="col-md-6">
-                            <div class="form-group">
-								<h5>Nama <span class="text-danger">*</span></h5>
-								<div class="controls">
-									<input type="text" name="textNama" class="form-control" required data-validation-required-message="This field is required"> </div>
-								<div class="form-control-feedback"><small>Add <code>required</code> attribute to field for required validation.</small></div>
-							</div>
-						</div>
+                            </div>
+                            
+                            </div>
+                        </div>
                         <!-- END row select -->
-                        </div>
-
-                        <div class="row">
-                            <div class="col-md-6">
-                            <div class="form-group">
-								<h5>Email <span class="text-danger">*</span></h5>
-								<div class="controls">
-									<input type="email" name="email" class="form-control" required data-validation-required-message="This field is required"> </div>
-							</div>
-                            </div>
-                            
-                            <div class="col-md-6">
-                            <div class="form-group">
-								<h5>Password <span class="text-danger">*</span></h5>
-								<div class="controls">
-									<input type="password" name="password" class="form-control" required data-validation-required-message="This field is required"> </div>
-							</div>
-                            
-                            </div>
-                        </div>
+                        
                         <!-- end row -->
 						<div class="text-xs-right">
 							<button type="submit" class="btn btn-rounded btn-info">Submit</button>
