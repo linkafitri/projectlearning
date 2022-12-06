cc046fbe791f2294b01ffdf115fd4ab704f4cc8f
ernakha <ernakhasanah78@gmail.com>
2022-12-06 11:21:29 +0700

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,99 +1,87 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="utf-8">
+
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="{{asset('backend/images/favicon.ico')}}">
 
-    <title>Lab TI - Log in </title>
-  
-	<!-- Vendors Style-->
-	<link rel="stylesheet" href="{{asset('backend/css/vendors_css.css')}}">
-	  
-	<!-- Style-->  
-	<link rel="stylesheet" href="{{asset('backend/css/style.css')}}">
-	<link rel="stylesheet" href="{{asset('backend/css/skin_color.css')}}">	
+    <title>SB Admin 2 - Login</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="{{asset('backend/css/all.min.css')}}" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="{{asset('backend/css/sb-admin-2.min.css')}}" rel="stylesheet">
 
 </head>
-<body class="hold-transition theme-primary bg-gradient-primary">
-	
-	<div class="container h-p100">
-		<div class="row align-items-center justify-content-md-center h-p100">	
-			
-			<div class="col-12">
-				<div class="row justify-content-center no-gutters">
-					<div class="col-lg-4 col-md-5 col-12">
-						<div class="content-top-agile p-10">
-							<h2 class="text-white">Get started with Us</h2>
-							<p class="text-white-50">Sign in to start your session</p>							
-						</div>
-						<div class="p-30 rounded30 box-shadowed b-2 b-dashed">
-							<form action="{{ route('login') }}" method="post">
-                                @Csrf
-								<div class="form-group">
-									<div class="input-group mb-3">
-										<div class="input-group-prepend">
-											<span class="input-group-text bg-transparent text-white"><i class="ti-user"></i></span>
-										</div>
-										<input type="email" id="email" name="email" class="form-control pl-15 bg-transparent text-white plc-white" placeholder="Email">
-									</div>
-								</div>
-								<div class="form-group">
-									<div class="input-group mb-3">
-										<div class="input-group-prepend">
-											<span class="input-group-text  bg-transparent text-white"><i class="ti-lock"></i></span>
-										</div>
-										<input type="password" id="password" name="password" class="form-control pl-15 bg-transparent text-white plc-white" placeholder="Password">
-									</div>
-								</div>
-								  <div class="row">
-									<div class="col-6">
-									  <div class="checkbox text-white">
-										<input type="checkbox" id="remember_me" name="remember" >
-										<label for="basic_checkbox_1">Remember Me</label>
-									  </div>
-									</div>
-									<!-- /.col -->
-									<div class="col-6">
-									 <div class="fog-pwd text-right">
-										<a href="{{ route('password.request') }}" class="text-white hover-info"><i class="ion ion-locked"></i> Forgot pwd?</a><br>
-									  </div>
-									</div>
-									<!-- /.col -->
-									<div class="col-12 text-center">
-									  <button type="submit" class="btn btn-info btn-rounded mt-10">SIGN IN</button>
-									</div>
-									<!-- /.col -->
-								  </div>
-							</form>														
 
-							<div class="text-center text-white">
-							  <p class="mt-20">- Sign With -</p>
-							  <p class="gap-items-2 mb-20">
-								  <a class="btn btn-social-icon btn-round btn-outline btn-white" href="#"><i class="fa fa-facebook"></i></a>
-								  <a class="btn btn-social-icon btn-round btn-outline btn-white" href="#"><i class="fa fa-twitter"></i></a>
-								  <a class="btn btn-social-icon btn-round btn-outline btn-white" href="#"><i class="fa fa-google-plus"></i></a>
-								  <a class="btn btn-social-icon btn-round btn-outline btn-white" href="#"><i class="fa fa-instagram"></i></a>
-								</p>	
-							</div>
-							
-							<div class="text-center">
-								<p class="mt-15 mb-0 text-white">Don't have an account? <a href="{{route('register')}}" class="text-info ml-5">Sign Up</a></p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+<body class="bg-gradient-primary">
 
+    <div class="container">
 
-	<!-- Vendor JS -->
-	<script src="{{asset('backend/js/vendors.min.js')}}"></script>
-    <script src="{{asset('../assets/icons/feather-icons/feather.min.js')}}"></script>	
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-xl-10 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                    </div>
+                                    <form class="user" method="POST" action="{{ route('login') }}">
+										@csrf
+                                        <div class="form-group">
+                                            <input type="email" class="form-control form-control-user"
+                                                id="email" name="email" aria-describedby="emailHelp"
+                                                placeholder="Enter Email Address...">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control form-control-user"
+                                                id="password" name="password" placeholder="Password">
+                                        </div>
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">
+											Login
+										</button>
+                                    </form>
+                                    <hr>
+                                    <div class="text-center">
+                                        <a class="small" href="{{route('register')}}">Create an Account!</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{asset('backend/js/jquery.min.js')}}"></script>
+    <script src="{{asset('backend/js/bootstrap.bundle.min.js')}}"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="{{asset('backend/js/jquery.easing.min.js')}}"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="{{asset('backend/js/sb-admin-2.min.js')}}"></script>
 
 </body>
+
 </html>
