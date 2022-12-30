e2e1cc5df90c77c5d521c37b516897bb3e86abc5
linkafitri <linkafitri@gmail.com>
2022-12-30 09:00:50 +0700

    new

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -1,87 +1,80 @@
 <aside class="main-sidebar">
-    <!-- sidebar-->
-    <section class="sidebar">	
-		
-        <div class="user-profile">
-			<div class="ulogo">
-				 <a href="index.html">
-				  <!-- logo for regular state and mobile devices -->
-					 <div class="d-flex align-items-center justify-content-center">					 	
-						  <img src="{{asset('backend/images/logo-dapoerumma.jpeg')}}" alt="">
-						  <h3><b>Dapoer</b> Umma</h3>
-					 </div>
-				</a>
-			</div>
-        </div>
-      
-      <!-- sidebar menu-->
-      <ul class="sidebar-menu" data-widget="tree">  
-		  
-		<li>
-          <a href="index.html">
-            <i data-feather="pie-chart"></i>
-			<span>Dashboard</span>
-          </a>
-        </li>  
-		
-        <li class="treeview">
-          <a href="#">
-            <i data-feather="message-circle"></i>
-            <span>Kelola User</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-right pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="{{route('user.view')}}"><i class="ti-more"></i>Tampilkan User</a></li>
-            <li><a href="{{route('user.add')}}"><i class="ti-more"></i>Tambah User</a></li>
-          </ul>
-        </li> 
-		  
-        <li class="treeview">
-          <a href="#">
-            <i data-feather="tag"></i>
-            <span>Kelola Produk</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-right pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="{{route('produk.view')}}"><i class="ti-more"></i>Tampilkan Produk</a></li>
-            <li><a href="{{route('produk.add')}}"><i class="ti-more"></i>Tambah Produk</a></li>
-          </ul>
-        </li>
+  <!-- sidebar-->
+  <section class="sidebar">
 
-        <li class="treeview">
-          <a href="#">
-            <i data-feather="archive"></i>
-            <span>Kelola Paket</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-right pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="{{route('paket.view')}}"><i class="ti-more"></i>Tampilkan Paket</a></li>
-            <li><a href="{{route('paket.add')}}"><i class="ti-more"></i>Tambah Paket</a></li>
-          </ul>
-        </li>
-		  
-		<li>
-          <a href="{{route('admin.logout')}}">
-            <i data-feather="lock"></i>
-			<span>Log Out</span>
-          </a>
-        </li> 
-        
-      </ul>
-    </section>
-	
-	<div class="sidebar-footer">
-		<!-- item-->
-		<a href="javascript:void(0)" class="link" data-toggle="tooltip" title="" data-original-title="Settings" aria-describedby="tooltip92529"><i class="ti-settings"></i></a>
-		<!-- item-->
-		<a href="mailbox_inbox.html" class="link" data-toggle="tooltip" title="" data-original-title="Email"><i class="ti-email"></i></a>
-		<!-- item-->
-		<a href="javascript:void(0)" class="link" data-toggle="tooltip" title="" data-original-title="Logout"><i class="ti-lock"></i></a>
-	</div>
-  </aside>
+    <div class="user-profile">
+      <div class="ulogo">
+        <a href="index.html">
+          <!-- logo for regular state and mobile devices -->
+          <div class="d-flex align-items-center justify-content-center">
+            <img src="{{asset('backend/images/logo-dapoerumma.jpeg')}}" alt="">
+            <h3><b>Dapoer</b> Umma</h3>
+          </div>
+        </a>
+      </div>
+    </div>
+
+    <!-- sidebar menu-->
+    <ul class="sidebar-menu" data-widget="tree">
+
+      <li class="treeview">
+        <a href="#">
+          <i data-feather="message-circle"></i>
+          <span>Kelola User</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-right pull-right"></i>
+          </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="{{route('user.view')}}"><i class="ti-more"></i>Tampilkan User</a></li>
+          <li><a href="{{route('user.add')}}"><i class="ti-more"></i>Tambah User</a></li>
+        </ul>
+      </li>
+
+      <li class="treeview">
+        <a href="#">
+          <i data-feather="tag"></i>
+          <span>Kelola Produk</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-right pull-right"></i>
+          </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="{{route('produk.view')}}"><i class="ti-more"></i>Tampilkan Produk</a></li>
+          <li><a href="{{route('produk.add')}}"><i class="ti-more"></i>Tambah Produk</a></li>
+        </ul>
+      </li>
+
+      <li class="treeview">
+        <a href="#">
+          <i data-feather="archive"></i>
+          <span>Kelola Paket</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-right pull-right"></i>
+          </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="{{route('paket.view')}}"><i class="ti-more"></i>Tampilkan Paket</a></li>
+          <li><a href="{{route('paket.add')}}"><i class="ti-more"></i>Tambah Paket</a></li>
+        </ul>
+      </li>
+
+      <li>
+        <a href="{{route('admin.logout')}}">
+          <i data-feather="lock"></i>
+          <span>Log Out</span>
+        </a>
+      </li>
+
+    </ul>
+  </section>
+
+  <div class="sidebar-footer">
+    <!-- item-->
+    <a href="javascript:void(0)" class="link" data-toggle="tooltip" title="" data-original-title="Settings" aria-describedby="tooltip92529"><i class="ti-settings"></i></a>
+    <!-- item-->
+    <a href="mailbox_inbox.html" class="link" data-toggle="tooltip" title="" data-original-title="Email"><i class="ti-email"></i></a>
+    <!-- item-->
+    <a href="javascript:void(0)" class="link" data-toggle="tooltip" title="" data-original-title="Logout"><i class="ti-lock"></i></a>
+  </div>
+</aside>
